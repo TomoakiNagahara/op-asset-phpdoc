fd2089a8c763065fe65822962f0f42f93b583a2e
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-10-02 16:54:06 +0900

    Add: index.php | PHPDoc

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**	op-asset-phpdoc:/index.php
+ *
+ * @created   2025-10-02
+ * @version   1.0
+ * @package   op-asset-phpdoc
+ * @author    Tomoaki Nagahara
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/**	declare
+ *
+ */
+declare(strict_types=1);
+
+/**	namespace
+ *
+ */
+namespace OP;
