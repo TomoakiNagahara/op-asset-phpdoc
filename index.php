55501e67dbc9e2f0282fd6b40646b42167e5cabe
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-10-02 16:56:04 +0900

    Add: index.php | Setup steps.

--- index.php
+++ index.php
@@ -17,3 +17,34 @@ declare(strict_types=1);
  *
  */
 namespace OP;
+
+//	...
+if( file_exists('index.html') ){
+	include('index.html');
+	return;
+}
+
+?>
+<section data-translation="true">
+	<h1>Reference of the ONEPIECE Framework</h1>
+	<p>
+		The API reference has not been generated yet.
+		To generate the reference, follow the steps below:
+	</p>
+	<ul>
+		<li>
+			Open the terminal application.
+		</li>
+		<li>
+			Change the current directory:
+			<code>cd <?= __DIR__ ?></code>
+		</li>
+		<li>
+			Run the following command:
+			<code>php ./generate.php</code>
+		</li>
+		<li>
+			After the process is complete, reload this page.
+		</li>
+	</ul>
+</section>
